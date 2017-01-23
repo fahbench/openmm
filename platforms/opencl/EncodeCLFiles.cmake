353bd7656c39b8a4d91aafe8a4d8a4883bdf731f
Matthew Harrigan <harrigan@stanford.edu>
2017-01-23 14:45:18 -0800

    openmm changes

--- platforms/opencl/EncodeCLFiles.cmake
+++ platforms/opencl/EncodeCLFiles.cmake
@@ -1,7 +1,5 @@
-FILE(GLOB OPENCL_KERNELS ${CL_SOURCE_DIR}/kernels/*.cl)
-SET(CL_FILE_DECLARATIONS)
-SET(CL_FILE_DEFINITIONS)
-CONFIGURE_FILE(${CL_SOURCE_DIR}/${CL_SOURCE_CLASS}.cpp.in ${CL_KERNELS_CPP})
+FILE(GLOB OPENCL_KERNELS ${SRC_DIR}/src/kernels/*.cl)
+CONFIGURE_FILE(${SRC_DIR}/src/OpenCLKernelSources.cpp.in OpenCLKernelSources.cpp)
 FOREACH(file ${OPENCL_KERNELS})
     # Load the file contents and process it.
     FILE(STRINGS ${file} file_content NEWLINE_CONSUME)
@@ -15,13 +13,14 @@ FOREACH(file ${OPENCL_KERNELS})
     STRING(REPLACE "\n" "\\n\"\n\"" file_content "${file_content}")
 
     # Determine a name for the variable that will contain this file's contents
-    FILE(RELATIVE_PATH filename ${CL_SOURCE_DIR}/kernels ${file})
+    FILE(RELATIVE_PATH filename ${SRC_DIR}/src/kernels ${file})
     STRING(LENGTH ${filename} filename_length)
     MATH(EXPR filename_length ${filename_length}-3)
     STRING(SUBSTRING ${filename} 0 ${filename_length} variable_name)
 
     # Record the variable declaration and definition.
-    SET(CL_FILE_DECLARATIONS ${CL_FILE_DECLARATIONS}static\ const\ std::string\ ${variable_name};\n)
-    FILE(APPEND ${CL_KERNELS_CPP} const\ string\ ${CL_SOURCE_CLASS}::${variable_name}\ =\ \"${file_content}\"\;\n)
+	SET(CL_FILE_DECLARATIONS ${CL_FILE_DECLARATIONS}static\ const\ std::string\ ${variable_name};\n)
+    FILE(APPEND OpenCLKernelSources.cpp const\ string\ OpenCLKernelSources::${variable_name}\ =\ \"${file_content}\"\;\n)
 ENDFOREACH(file)
-CONFIGURE_FILE(${CL_SOURCE_DIR}/${CL_SOURCE_CLASS}.h.in ${CL_KERNELS_H})
+
+CONFIGURE_FILE(${SRC_DIR}/src/OpenCLKernelSources.h.in OpenCLKernelSources.h)
